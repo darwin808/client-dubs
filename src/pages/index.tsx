0fe322f822cba1dbb2f651b94478d4d8fe174da9
darwin <darwin.apolinario@serino.com>
2021-12-25 14:35:10 +0800

    feat(test): test
    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div className="bg-red-200 h-screen">estong</div>
+  return <div className="bg-red-200 h-screen">estonqweg</div>
 }
 
 export default Home
