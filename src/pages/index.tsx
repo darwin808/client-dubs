76f1040e403f9b8c54d18966ef7e298ff21acc65
darwin <darwin.apolinario@serino.com>
2021-12-26 21:45:58 +0800

    feat(merge): wqer
    qwe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-<<<<<<< HEAD
-  return <div className="bg-red-200 h-screen">masteri1: https://d28gz2fnk9q2vf.cloudfront.net/</div>
-=======
   return (
     <div className="bg-red-200 h-screen">
       <a href="/b" className="underline text-lg">
@@ -11,7 +8,6 @@ const Home: NextPage = () => {
       </a>
     </div>
   )
->>>>>>> a9d4cdf52a4e6aa6484b0956e777ca4c019d69cd
 }
 
 export default Home
