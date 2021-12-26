eba36528b3ee9b2f75625e3f0f7da7613867472a
darwin <darwin.apolinario@serino.com>
2021-12-26 16:33:22 +0800

    m1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div className="bg-red-200 h-screen">master: https://d28gz2fnk9q2vf.cloudfront.net/</div>
+  return <div className="bg-red-200 h-screen">masteri1: https://d28gz2fnk9q2vf.cloudfront.net/</div>
 }
 
 export default Home
