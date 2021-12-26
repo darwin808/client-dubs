5ebc2ef3760ea1ab9a37e79fcb3e7fa0585c9b4d
darwin <darwin.apolinario@serino.com>
2021-12-26 11:01:29 +0800

    feat(deploy): deplloy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <div className="bg-red-200 h-screen">estonqweg</div>
+  return <div className="bg-red-200 h-screen">darwin</div>
 }
 
 export default Home
